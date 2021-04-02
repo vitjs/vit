d44af90efbfc5a5c870cb2d9500c70041028b621
theprimone <yuns.xie@qq.com>
2021-04-02 22:34:17 +0800

    feat: export react-router-dom modules

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,2 +1,20 @@
+export {
+  Link,
+  NavLink,
+  Prompt,
+  Redirect,
+  Route,
+  Router,
+  StaticRouter,
+  MemoryRouter,
+  Switch,
+  matchPath,
+  withRouter,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from 'react-router-dom';
+
 export { default as renderRoutes } from './renderRoutes';
 export { default as dynamic } from './dynamic';
