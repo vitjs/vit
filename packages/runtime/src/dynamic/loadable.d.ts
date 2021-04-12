e09296d608ec804fd9e70c08b50c7750b75cf62e
theprimone <yuns.xie@qq.com>
2021-04-12 21:17:50 +0800

    refactor: loadable

---
+++ packages/runtime/src/dynamic/loadable.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+declare namespace LoadableExport {
+  interface ILoadable {
+    <P = {}>(opts: any): React.ComponentClass<P>;
+    Map<P = {}>(opts: any): React.ComponentType<P>;
+    preloadAll(): Promise<any>;
+  }
+}
+
+declare const LoadableExport: LoadableExport.ILoadable;
+
+export = LoadableExport;
